5993fdec6f0bb4eba03c500c24fffbc6ddb0cab8
Luis Enrique <luiskike2210@gmail.com>
2024-03-05 12:01:25 -0600

    13 Images

--- lib/screens/image_screens.dart
+++ lib/screens/image_screens.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/theme/app_theme.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -14,12 +16,64 @@ class _ImageScreenState extends State<ImageScreen> {
       appBar: AppBar(
         title: const Text('Imagenes'),
       ),
-      body: const Column(
+      body:  ListView(
         children: [ 
-          Image(
-          image: AssetImage('assets/img/blue_mosque.jpg'),)
+         cardImage1(),
+         imageFade(),
         ],
       )
+    );
+  }
+  Card cardImage1(){
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
+      ),
+      margin:const EdgeInsets.all(15),
+      elevation: 10,
+      borderOnForeground: true,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+      child:Column(
+        children: [
+          const SizedBox(
+            height: 400.0,
+            width: 600.0,
+            child:  Image(
+            image: AssetImage('assets/img/blue_mosque.jpg'),
+            ),
+          ),
+          Container(
+            padding:const EdgeInsets.all(10),
+            child: Text(
+              'Edificio en estambul',
+              style: AppTheme.lightTheme.textTheme.headlineMedium,
+            ),
+          ),
+        ],
+      ),
+      )
+    );
+  }
+  SizedBox image2(){
+    return SizedBox(
+      height: 200.0,
+      width: 400.0,
+      child: Image.network('https://spain.id.nba.com/storage/images/banners/social_1665757506.jpg'),
+    );
+  }
+  Stack imageFade(){
+    return Stack(
+      children: <Widget>[
+        const Center(
+          child: CircularProgressIndicator(),
+        ),
+        Center(
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image: 'https://spain.id.nba.com/storage/images/banners/social_1665757506.jpg'
+            ),
+        ),
+      ],
     );
   }
 }
